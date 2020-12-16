f447a18e6f74e01ca2563af87c79fc06b173a599
Dimitri <dimitri.bourlier@gmail.com>
2020-12-16 17:48:23 +0100

    Merge branch 'master' of https://github.com/BenDev78/projet_soutenance into bourlierdimitri_161220
    # Conflicts:
    #	src/DataFixtures/AppFixtures.php

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,13 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
+        ->add('lastname', TextType::class, [
+            'label' => 'Nom'
+        ])
+        ->add('email', EmailType::class)
+        ->add('password', PasswordType::class)
         ;
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
