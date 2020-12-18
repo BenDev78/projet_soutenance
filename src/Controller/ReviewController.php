fdbf692dca03362d51e6cf2ef196224a93ad837f
Mathilde GLET <mathilde_glet78@orange.fr>
2020-12-18 14:51:29 +0100

    enregistrement formulaire review en bdd

---
+++ src/Controller/ReviewController.php
@@ -0,0 +1,76 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Product;
+use App\Entity\Review;
+use App\Entity\User;
+use App\Form\ReviewType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReviewController extends AbstractController
+{
+
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    # Faire modification de la route une fois qu'on aura les produits => "/product{id}/review" #
+
+    /**
+     * Création du formulaire d'avis produit
+     * @Route("/review/{id}", name="product_review", methods={"GET|POST"})
+     * @param Request $request
+     * @param Product $product
+     * @return Response
+     */
+    public function review(Request $request, Product $product): Response
+    {
+        #Remplacer "$user = $this->>getDoctrine etc" par ligne ci-dessous lorsque les logins seront fonctionnels
+        #$user = $this->getUser();
+        $user = $this->getDoctrine()->getRepository(User::class)->find(1);
+        $review = new Review();
+        $review->setProduct($product);
+        $review->setUser($user);
+
+        $form = $this->createForm(ReviewType::class, $review);
+        $form -> handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            if($review->getPseudo() == null)
+            {
+                $review->setPseudo("Un utilisateur");
+            }
+
+            $this->em->persist($review);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Votre avis a bien été envoyé, merci!');
+
+            return $this->redirectToRoute("shop_product");
+
+        }
+
+        return $this->render('review/formReviews.html.twig', ['form' => $form->createView()]);
+
+    }
+
+    #TODO
+    #Nullable=true ne fonctionne pas pour le pseudo
+    #Afficher le nom du produit en haut du formulaire dans la vue
+    #Afficher le addFlash à la soumission du formulaire
+    #Ajouter le lien du formulaire review sur l'ensemble des produits
+
+
+}
