9a1bae6d7ce1d732e830c79702fd8e0b6b7c271b
Dimitri <dimitri.bourlier@gmail.com>
2020-12-23 17:41:40 +0100

    23/12/20

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -54,7 +55,11 @@ class User implements UserInterface
 
 
     /**
-     * @ORM\Column(type="string", length=10, nullable=true)
+     * @ORM\Column(type="string", nullable=true)
+     * @assert
+     * @Assert\Regex(
+     *  pattern="/[0-9]{10}/"
+     *  )
      */
     private $phone;
 
--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -5,12 +5,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
+
 
 class ProfileType extends AbstractType
 {
@@ -34,7 +34,6 @@ class ProfileType extends AbstractType
                 ]
             ])
             ->add('phone', TelType::class, [
-                'constraints'=>new Length(10),
                 'attr' =>[
                     'placeholder' => 'Merci de saisir votre téléphone'
                 ]
